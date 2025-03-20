8256cd3298853c7af9335b0d8d4920d18cd5912c
Yaw Mintah <yawmintah1@gmail.com>
2025-03-20 21:04:31 +0000

    initial commit for accounts app

---
+++ accounts/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'        # This is the field that is used for login
+    REQUIRED_FIELDS = ['username'] 
+
+
+    def __str__(self):
+        return self.email
+    
+
